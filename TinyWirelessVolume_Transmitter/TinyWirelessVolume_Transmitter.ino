176f416f968ef53cc4a9f871915de3ba7ca53930
Fermín Olaiz <ferminolaiz@gmail.com>
2016-12-24 01:45:18 -0300

    Fail, VirtualWire already does it all

--- TinyWirelessVolume_Transmitter/TinyWirelessVolume_Transmitter.ino
+++ TinyWirelessVolume_Transmitter/TinyWirelessVolume_Transmitter.ino
@@ -1,7 +1,13 @@
 /**
  * Thanks to
+ * 
+ * Rotary encoder ideas: 
  * https://thewanderingengineer.com/2014/08/11/rotary-encoder-on-the-attiny85-part-2/
  * http://bildr.org/2012/08/rotary-encoder-arduino/
+ * http://www.buxtronix.net/2011/10/rotary-encoders-done-properly.html
+ * 
+ * RF and VirtualWire info: 
+ * http://www.lydiard.plus.com/hardware_pages/433mhz_modules.htm
  */
 
 	// RF
@@ -33,15 +39,11 @@
 
 		#define RF_TX_PIN 1
 
-		// Most of the times with a lower bitrate the RF module's range is better, but, somehow, with lower speeds it works worse
+		// Most of the times with a lower bitrate the RF module's range is better
 		// If it doesn't work, try changing it to a lower value. Don't forget to change it in the receiver side
 		#define RF_BITRATE 600
 
-		#define RF_PREAMBLE_LENGTH 2
-		#define RF_PREAMBLE 0xaa, 0x55
-
 	// The encoder common pin is tied to ground, then, we need to enable the pullup resistors
-
 	#define ENABLE_PULLUPS
 
 	// Shared variables used by the interrupt
@@ -82,22 +84,22 @@
 		{
 			// Send MessageID (random) and button press command (0x11)
 
-			char Data[] = {RF_PREAMBLE, random(255), 0x11};
-			vw_send((uint8_t*) Data, RF_PREAMBLE_LENGTH + 2);
+			char Data[] = {random(255), 0x11};
+			vw_send((uint8_t*) Data, 2);
 		}
 
 		if(EncoderValue != 0 && !vx_tx_active())
 		{
 			// Send MessageID (random), encoder change command (0x10) and EncoderValue (int16_t, two 8-bit bytes)
 
-			char Data[] = {RF_PREAMBLE, random(255), 0x10, EncoderValue >> 8, EncoderValue & 0xFF};
+			char Data[] = {random(255), 0x10, EncoderValue >> 8, EncoderValue & 0xFF};
 
 			// We'll send it and we'll not wait
 			// The main loop will still be listening for button pulses and, when the tx 
 			// becomes available, and there's something to send, it will send it. 
 			// We can't send the same message twice because if we senda message followed
 			// by another, the library will vw_tx_wait() before sending the second one. 
-			vw_send((uint8_t*) Data, RF_PREAMBLE_LENGTH + 4);
+			vw_send((uint8_t*) Data, 4);
 
 			// Reset Encoder counter to 0
 			EncoderValue = 0;
